75fe8c3149eb9e601061f7eea4fe345906404a59
blen-abebe <your_email@example.com>
2025-06-16 09:20:55 -0500

    Initial commit: clustering and MSM analysis

--- clusteringEnv.py
+++ clusteringEnv.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
-from sklearn.neighbors import NearestNeighbors
+import hdbscan
+import pyemma.msm as msm
+
 
 #load the data + pair the values to create a 2D array 
 tilt = np.loadtxt('Rheb_GTPtiltangles.txt')
@@ -12,6 +13,16 @@ assert len(tilt) == len(rotation), "Tilt and rotation arrays must have the same 
 X = np.column_stack((tilt, rotation))
 print(X)
 print(X.shape)
+
+#convert tilt and rotation from degrees to radians
+tilt_rad = np.deg2rad(tilt)
+rotation_rad = np.deg2rad(rotation)
+
+X_circ = np.column_stack([
+    np.cos(tilt_rad), np.sin(tilt_rad),
+    np.cos(rotation_rad), np.sin(rotation_rad)
+])
+
 
 #plot data to see how it looks 
 angles = np.loadtxt('Rheb_GTProtationangles.txt') 
@@ -28,33 +39,29 @@ plt.ylabel('Rotation (deg)')
 plt.title('Tilt vs Rotation')
 plt.show()
 
-#DBSCAN (density-based clustering alg) --> lables points that don't belong to any cluster as -1, can find any shaped clusters + does not require you to speciy the num in advance 
-# distance from each point to its k-th neigbour in data set
-k = 10
-neigh = NearestNeighbors(n_neighbors=k)
-nbrs = neigh.fit(X)
-distances, indices = nbrs.kneighbors(X)
+#merges clusters based on distance between points, does not require you to specify the number of clusters in advance
+# you can choose the number of clusters you want to find, e.g., n_clusters=3
+# Convert angles to radians
 
-# Sort the distances to the k-th nearest neighbor
-k_distances = np.sort(distances[:, k-1])
+step = 30
+X_sub = X[::step]
+tilt_sub = tilt[::step]
+rotation_sub = rotation[::step]
+X_circ_sub = X_circ[::step]
 
-plt.figure()
-plt.plot(k_distances)
-plt.xlabel('Points sorted by distance')
-plt.ylabel(f'{k}th nearest neighbor distance')
-plt.title('k-distance plot for DBSCAN eps selection')
-plt.show() # sharp change occurs around 1.5-2 so that will be the eps valye ir the neighborhood radius for clustering 
+clusterer = hdbscan.HDBSCAN(min_cluster_size=90)
+labels = clusterer.fit_predict(X_circ_sub)
 
-#eps --> how close points need to be to be considered the cluster (radius) 
-#min samples --> min num of points to form a dense region  
-db = DBSCAN(eps=2, min_samples=10).fit(X)
-labels = db.labels_
-plt.scatter(tilt, rotation, c=labels, cmap='tab10', s=10)
+plt.scatter(tilt_sub, rotation_sub, c=labels, cmap='tab10', s=10)
 plt.xlabel('Tilt (deg)')
 plt.ylabel('Rotation (deg)')
-plt.title('DBSCAN Clustering (Circular Features)')
+plt.title('HDBSCAN Clustering [Circular Features, 3 Clusters]')
 plt.show()
 
-#num of clusters, -1 rep noise points
+# Print number of clusters found (excluding noise, labeled as -1)
 n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-print(f"Number of clusters found: {n_clusters}") #11 clusters found
+print(f"Number of clusters found: {n_clusters}")
+
+#pyemma markov state model
+#source pyemma-env/bin/activate
+#python clusteringEnv.py
